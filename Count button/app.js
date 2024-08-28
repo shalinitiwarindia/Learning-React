c251e9f989b8b2a8545ac50ea3f68983e888b481
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-28 22:01:33 +0530

    Create app.js

---
+++ Count button/app.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  const [counter, setCounter] = useState(10);
+  return (
+    <div className="App">
+      <h3>Counter:{counter}</h3>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Add 1
+      </button>
+    </div>
+  );
+}
