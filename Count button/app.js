cc7bbc3c6542677f9c7d1710de9f45f87ccef17d
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-29 21:51:34 +0530

    Update app.js

--- Count button/app.js
+++ Count button/app.js
@@ -1,9 +1,3 @@
-import "./styles.css";
-import { useState } from "react";
-export default function App() {
-  const [counter, setCounter] = useState(0);
-  return (
-    <div className="App">
-      <h3>Counter:{counter}</h3>
+
       
    
