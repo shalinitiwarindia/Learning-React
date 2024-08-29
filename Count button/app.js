a9039185df4ca60dfe57503d2c48a6b864296e0f
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-29 21:51:47 +0530

    Update app.js

--- Count button/app.js
+++ Count button/app.js
@@ -1,3 +1,32 @@
-
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <h3>Counter:{counter}</h3>
+      <button
+        onClick={() => {
+          if (counter >= 10) {
+            return;
+          }
+          setCounter(counter + 1);
+        }}
+      >
+        Add 1
+      </button>
+      <button
+        onClick={() => {
+          if (counter < 10) {
+            return;
+          }
+          setCounter(counter - 1);
+        }}
+      >
+        Sub 1
+      </button>
+    </div>
+  );
+}
       
    
