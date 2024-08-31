680142c14e50398565f8087a58efcb4ba69ef271
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-31 17:39:16 +0530

    Create TodoInput.jsx

---
+++ ToDo app/src/components/TodoInput.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const TodoInput = () => {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          //   console.log(e.target.value);
+          setText(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Todo"
+      />
+      <button>Add Todo</button>
+      {text}
+    </div>
+  );
+};
