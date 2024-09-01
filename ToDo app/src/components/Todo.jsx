0b171049524d12bfd93f9bc76d4509e535d48f64
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-09-01 21:27:56 +0530

    Update Todo.jsx

--- ToDo app/src/components/Todo.jsx
+++ ToDo app/src/components/Todo.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { TodoInput } from "./TodoInput";
 
 function Todo() {
+  const [todosList, setTodosList] = useState(["Learn React", "learn Redux"]);
   return (
     <div>
       <TodoInput />
+      {todosList.map((e) => (
+        <div>{e}</div>
+      ))}
     </div>
   );
 }
