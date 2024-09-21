5b594b1a6bd63e7a1d9c71975135fefc8f5f46f8
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-09-21 21:13:52 +0530

    Create Todos.jsx

---
+++ Todo by useEffect/Todos.jsx
@@ -0,0 +1,47 @@
+import { useEffect,useState } from "react";
+export const Todos = () => {
+    const [todos,setTodos]=useState([]);
+    const [text,setText] = useState("");
+    const [loading,setLoading]=useState(true);
+    useEffect(()=>{
+        getData();
+
+    },[]);
+    const getData=async () => {
+        const data = await  fetch("http://localhost:8080/todos")
+        .then((d) => d.json());
+      
+          setTodos(data);
+          setLoading(false);
+        };
+        return loading ? ("Loading....." ):(
+            <div>
+                <input 
+                onChange={(e) => setText(e.target.value)}
+                type="text"
+                placeholder="Enter todo" />
+                <button 
+                onClick={()=>{
+                    const payload ={
+                      title:text,
+                      status:false,  
+                    };
+                    fetch("http://localhost:8080/todos",{
+                        method:"POST",
+                        headers:{
+                            "content-type": "application/json",
+                        },
+                        body: JSON.stringify(payload),
+                    }).then(()=>{
+                       getData(); 
+                    });
+                }}>Add todo</button>
+                {todos.map((t)=>(
+                    <div>{t.title}</div>
+                ))}
+            </div>
+        );
+    
+    };
+
+
