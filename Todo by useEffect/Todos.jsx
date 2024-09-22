53bbe67086e14d4d444e2c32832c8b71f56874d9
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-09-22 20:55:56 +0530

    Update Todos.jsx

--- Todo by useEffect/Todos.jsx
+++ Todo by useEffect/Todos.jsx
@@ -1,10 +1,1 @@
-import { useEffect,useState } from "react";
-export const Todos = () => {
-    const [todos,setTodos]=useState([]);
-    const [text,setText] = useState("");
-    const [loading,setLoading]=useState(true);
-    useEffect(()=>{
-        getData();
 
-    },[]);
-  
