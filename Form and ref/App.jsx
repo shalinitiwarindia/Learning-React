157261e83fb7a4a105c29cc182172a2ac8d39c56
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-09-22 21:22:01 +0530

    Create App.jsx

---
+++ Form and ref/App.jsx
@@ -0,0 +1,25 @@
+
+import { useEffect, useState } from 'react'
+import {Hello} from './components/Hello';
+import { Todos } from './components/Todos';
+import './App.css'
+import { Counter } from './components/Counter';
+import { Form } from './components/Form';
+
+
+function App() {
+const [show,setShow]=useState(true);
+  return (
+    <div className="App">
+      <Form/>
+     {/* <Counter />
+     {show ? <Todos /> : null}
+     <button onClick={() => {
+      setShow(!show)
+     }}>{show ? "Hide" : "Show"}</button> */}
+
+    </div>
+  );
+}
+
+export default App;
