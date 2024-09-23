8fbbe67070db44b93dd09c043ad16ff8002aba18
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-09-23 20:48:41 +0530

    Update Counter.jsx

--- Timer/Counter.jsx
+++ Timer/Counter.jsx
@@ -1,1 +1,27 @@
+import { useEffect, useState } from "react"
+
+
+export const Counter = () =>{
+    const [count,setCount] = useState(10);
+    // setInterval(() => {
+    //     setCount(count-1);
+    // },1000);
+    useEffect(() =>{
+        let id = setInterval(() => {
+            setCount((prevValue) => {
+                if(prevValue<=0){
+                  clearInterval(id);  
+                  return 0;
+                }
+                return prevValue - 1;
+            });
+        },1000);
+    },[]);
+
+    return (
+        <div>
+            <h3>Count: {count}</h3>
+        </div>
+    );
+};
 
