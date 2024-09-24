dbd8a4e75d4b906a86543a22646b82c06f670574
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-09-24 19:12:56 +0530

    Create Button.jsx

---
+++ Styled Component/Style Component example/Components/Button.jsx
@@ -0,0 +1,9 @@
+// import styled from "styled-components";
+// const Button = styled.button`
+// backgrounmd-color:red;
+// `;
+// export {Button};
+import "./Button.css";
+export const Button = ()=> {
+    return <button className="btn">Add One</button>
+}
