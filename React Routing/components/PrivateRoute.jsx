5d1c12205308654d0471adbf3917b5857adcb73e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-01 22:38:05 +0530

    Create PrivateRoute.jsx

---
+++ React Routing/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthContext"
+import { Navigate } from "react-router-dom";
+
+export const PrivateRoute = ({children}) => {
+    const {isAuth}= useContext(AuthContext);
+
+    if(!isAuth){
+        return <Navigate to="/login" replace={true}></Navigate>
+    }
+    return children;
+};
