1e65dee76774055a1e71edb9efb12f8f2967c9ad
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-01 22:36:34 +0530

    Update UserDetails.jsx

--- React Routing/components/UserDetails.jsx
+++ React Routing/components/UserDetails.jsx
@@ -1,6 +1,26 @@
-import { useParams } from "react-router-dom"
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router-dom"
+import { AuthContext } from "../contexts/AuthContext";
 
 export const UserDetails = () => {
     const {id} = useParams();
-    return <div>User ID: {id}</div>;
+    const [user,setUser] = useState({});
+    const {isAuth}=useContext(AuthContext);
+
+    useEffect(() =>{
+        axios.get(`https://reqres.in/api/users/${id}`).then(({data}) => {
+            setUser(data.data);
+        })
+    },[]);
+    if(!isAuth){
+      return <Navigate to={"/login"} />;  
+    }
+
+    return <div>
+        <img src={user.avatar} alt="" />
+        <div>First Name: {user.first_name}</div>
+        <div>Last Name: {user.last_name}</div>
+    
+    </div>
 }
