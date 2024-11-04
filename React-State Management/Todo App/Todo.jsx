fc015bd5a045e7db241c12d6714a39f9143bbb85
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-04 15:42:50 +0530

    Update Todo.jsx

--- React-State Management/Todo App/Todo.jsx
+++ React-State Management/Todo App/Todo.jsx
@@ -1,16 +1,28 @@
+import { nanoid } from "nanoid";
 import { useState } from "react";
+import { TodoData } from "./TodoData";
 import { TodoInput } from "./TodoInput";
 
 export const Todo = () => {
   const [todosList, setTodosList] = useState([]);
   const getData = (todo) => {
-    setTodosList([...todosList, todo]);
+    const payload = {
+      title: todo,
+      status: true,
+      id: nanoid(5),
+    };
+    setTodosList([...todosList, payload]);
+  };
+  const handleStatus = (id) => {
+    setTodosList([
+      ...todosList.map((e) => (e.id === id ? { ...e, status: !e.status } : e)),
+    ]);
   };
   return (
     <div>
       <TodoInput getData={getData} />
       {todosList.map((e) => (
-        <div>{e}</div>
+        <TodoData handleStatus={handleStatus} todo={e}></TodoData>
       ))}
     </div>
   );
